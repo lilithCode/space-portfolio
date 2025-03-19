74706246e0c9f16ca442517302bf138b8f63b0ec
lilithCode <barbieatonline@gmail.com>
2025-03-19 08:47:20 +0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-  plugins: [react() ,tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  base: "/space-portfolio/",
+});
